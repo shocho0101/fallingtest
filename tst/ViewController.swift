36c2f29eef85a099153c5586e9740495609bde9d
keisukemitsumoto <ksk.mitsumoto.mj@gmail.com>
2015-11-25 20:49:54 +0900

    ラベル

--- tst/ViewController.swift
+++ tst/ViewController.swift
@@ -10,24 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var x: CGFloat = CGFloat()
-    var y: CGFloat = 100.0
+//    var x: CGFloat = CGFloat()
+    var xArray: [CGFloat] = [100.0,200.0,300.0,400.0] //x座標の配列
+    var y: CGFloat = 100.0 //y座標
     var timer: NSTimer!
-    var ytimer: NSTimer!
-    var testLabel: UILabel!
-    var counter: Float = 0.0
+    var testImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        
+        //二秒おきにImageViewを作る
         timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
         timer.fire()
-        
-        ytimer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdateY:", userInfo: nil, repeats: true)
-        ytimer.fire()
-        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,29 +31,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func makeLabel(){
-        testLabel = UILabel(frame: CGRectMake(0, 0, 40, 40))
-        testLabel.center = CGPointMake(x, y)//表示位置
-        testLabel.textAlignment = NSTextAlignment.Center
-//        testLabel.textColor = UIColor.blueColor()
-        testLabel.backgroundColor = UIColor.blueColor()
-        self.view.addSubview(testLabel)
-    }
-
-
-    func onUpdate(timer : NSTimer){
-
-        x = CGFloat(counter)
-        NSLog("x,y -> \(x)\(y)")
-        makeLabel()
-        counter += 50.0
-    }
     
-    func onUpdateY(timer: NSTimer){
-        var ycounter: CGFloat = 0.0
-        ycounter += 1.0
-        y = y + ycounter
+    //ImageViewを作る機能
+    func makeImageView(){
         
+        //ランダムの機能　0~3から一つを選ぶ
+        let randInt = Int(rand()%4)//0,1,2,3 の中から一つ
+        NSLog("\(randInt)")
+        
+        //iamgeViewのサイズ
+        testImageView = UIImageView(frame: CGRectMake(0, 0, 80, 80))
+        
+        //表示位置
+        testImageView.center = CGPointMake(xArray[randInt], y)
+        
+        //imageViewの色
+        testImageView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 0.3)
+        
+        //画面にimageViewを追加する
+        self.view.addSubview(testImageView)
     }
+
+
+    //二秒おきにmakeImageVIewって動作を行う
+    func onUpdate(timer : NSTimer){
+        makeImageView()
+    }
+
 }
 
